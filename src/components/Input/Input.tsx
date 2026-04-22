c7b169d7aed12344bc20aba69eb2127a08f9e509
AlexSMO <alex.fr.estudio@gmail.com>
2026-04-22 10:33:55 +0100

    feat: input component base

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,21 +1,33 @@
-import type React from "react";
+import type { InputProps } from "./Input.interface";
 import { inputStyles } from "./Input.styles";
-import type { VariantProps } from "class-variance-authority";
 import clsx from "clsx";
 
-type inputProps = {
-    children?: React.ReactNode,
-    className?: string
-} & VariantProps<typeof inputStyles>
 
-export const Input = ({ children, variant, className = "bg-amber-50 rounded-2xl border border-purple-50 focus:border-sky-400" } : inputProps)  => {
+export const Input = ({ 
+    variant,
+    placeholder,
+    helperText,
+    disabled,
+    leftIcon,
+    rightIcon,
+    onChange,
+    inputSize,
+    inputType,
+    state, 
+    className } 
+    : InputProps)  => {
 
     return (
-        <input
-        children = {children}
-        className={clsx(inputStyles({ variant }), className)}
-        > 
-        
-        </input>
+        <div>
+            {leftIcon}
+            <input
+            placeholder = {placeholder}
+            disabled = {disabled}
+            onChange={onChange}
+            className={clsx(inputStyles({ variant, inputSize, inputType, state }), className)}
+            /> 
+            {rightIcon}
+            {helperText}
+        </div>
     )
 }
