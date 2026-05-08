3e7fff0636431ea11e5006445b52a25578109a0f
AlexSMO <alex.fr.estudio@gmail.com>
2026-05-08 12:21:14 +0100

    fix: style conflict with state-variant

--- src/components/primitives/Textarea/Textarea.styles.ts
+++ src/components/primitives/Textarea/Textarea.styles.ts
@@ -1,37 +1,44 @@
 import { cva } from "class-variance-authority";
 
 export const TextareaStyles = cva(
-    `p-2 placeholder-gray-400 bg-gray-200/95 
-    rounded-md focus:outline-none focus:ring-2
-    shadow transition`
-    ,{
-        variants: {
+  `p-2 placeholder-gray-400 bg-gray-200/95 rounded-md focus:outline-none focus:ring-2 shadow transition`,
+  {
+    variants: {
+      variant: {
+        primary: "border border-gray-300 focus:ring-sky-400",
+        secondary: "border border-gray-400 focus:ring-black",
+        danger: "border border-gray-300 focus:ring-red-700",
+        ghost: "bg-transparent border border-gray-200 focus:ring-gray-400",
+      },
 
-            variant: {
-                primary: " focus:ring-sky-400 ",
-                secondary: "focus:ring-black",
-                danger: "focus:ring-red-700",
-                ghost: "bg-transparent focus:ring-gray-400"
-            },
-            
-            state: {
-                error: "bg-red-200",
-                success: "bg-green-200"
-            },
+      state: {
+        default: "",
+        error: "",
+        success: "",
+      },
 
-            size: {
-                sm: "w-48",
-                md: "w-64",
-                lg: "w-80"
-            },
-
-        },
-
-        defaultVariants: {
-            variant: "primary",
-            size: "md",
-        }
-
+      size: {
+        sm: "w-48",
+        md: "w-64",
+        lg: "w-80",
+      },
     },
 
+    compoundVariants: [
+      {
+        state: "error",
+        class: "border-red-500 bg-red-100 focus:ring-red-500",
+      },
+      {
+        state: "success",
+        class: "border-green-500 bg-green-100 focus:ring-green-500",
+      },
+    ],
+
+    defaultVariants: {
+      variant: "primary",
+      state: "default",
+      size: "md",
+    },
+  }
 );
